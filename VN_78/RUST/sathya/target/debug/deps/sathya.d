72522e2382b3ddcf5569faf5dfe8ef98707313da
VN_78 <vishwanath.s.god@gmail.com>
2024-09-05 14:49:20 +0530

    Initiated the project

---
+++ VN_78/RUST/sathya/target/debug/deps/sathya.d
@@ -0,0 +1,5 @@
+X:\PROJECTS\GIT_REPO\Tamil_vetri_kalagam\VN_78\RUST\sathya\target\debug\deps\sathya.exe: src/main.rs
+
+X:\PROJECTS\GIT_REPO\Tamil_vetri_kalagam\VN_78\RUST\sathya\target\debug\deps\sathya.d: src/main.rs
+
+src/main.rs:
---
+++ VN_78/RUST/sathya/target/debug/sathya.d
@@ -0,0 +1,1 @@
+X:\PROJECTS\GIT_REPO\Tamil_vetri_kalagam\VN_78\RUST\sathya\target\debug\sathya.exe: X:\PROJECTS\GIT_REPO\Tamil_vetri_kalagam\VN_78\RUST\sathya\src\main.rs
