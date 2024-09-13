6ba507158e523922660ce47098cdff3d65e27907
VN_78 <vishwanath.s.god@gmail.com>
2024-09-13 23:15:21 +0530

    coded a program to find the nth fibbonachi number

--- VN_78/RUST/branches/src/main.rs
+++ VN_78/RUST/branches/src/main.rs
@@ -1,12 +1,14 @@
-fn main() {
-    let mut counter = 0;
 
-    let result = loop {
-        counter +=1;
-        if counter == 10 {
-            break counter*2;
-        }
-    };
-    println!("{}", result);
+fn main () {
+    let fib = fib_num(8);
+    println!("{}", fib);
+}
 
+
+fn fib_num (n:u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib_num(n-1) + fib_num(n-2)
+    }
 }
